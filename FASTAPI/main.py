c8c67506d30ead8c2fe1f8b4918573a52ededb54
EstrellaCA <estrellacuellar3010@gmail.com>
2025-02-13 19:30:05 -0600

    endpoint
    agregar, actualizar y eliminar

--- FASTAPI/main.py
+++ FASTAPI/main.py
@@ -12,10 +12,10 @@ app = FastAPI(
 
 #Creamos una lista para la base de datos 
 usuarios=[
-    {'id':1, 'nombre':'Estrella', 'edad':20},
-    {'id':2, 'nombre':'Lu', 'edad':20},
-    {'id':3, 'nombre':'Lalo', 'edad':21},
-    {'id':4, 'nombre':'Domi', 'edad':20},
+    {"id":1, "nombre":"Estrella", "edad":20},
+    {"id":2, "nombre":"Lu", "edad":20},
+    {"id":3, "nombre":"Lalo", "edad":21},
+    {"id":4, "nombre":"Domi", "edad":20},
 ]
 
 
@@ -34,14 +34,32 @@ def ConsultarTodos():
 #endpont para agregar usuarios, la diagonal indica que es un parametro  
 @app.post('/usuarios/', tags=['Operaciones CRUD'])
 #definimos los parametros que recibira el metodo en este caso sera una lista de diccionarios
-def AgregarUsuario(usuarionuevo: dict):
+def AgregarUsuario(usuario:dict):
     for usr in usuarios:
-        if usr['id'] == usuarionuevo.get('id'):
+        if usr["id"] == usuario.get("id"):
             # el raise nos permite lanzar una excepcion
             raise HTTPException(status_code=400, detail='El id ya esta registrado')
     
-    usuarios.append(usuarionuevo)
-    return usuarionuevo
+    usuarios.append(usuario)
+    return usuario
+
+#endpoint para actualizar usuarios
+@app.put('/usuarios/{id}', tags=['Operaciones CRUD'])
+def ActualizarUsuaario(id:int, usuario:dict):
+    for usr in usuarios:
+        if usr["id"] == id:
+            usr.update(usuario)
+            return {"Usuario Actualizado":usr}
+    raise HTTPException(status_code=400, detail='Usuario no encontrado')
+
+#endpoint para eliminar usuarios
+@app.delete('/usuarios/{id}', tags=['Operaciones CRUD'])
+def EliminarUsuario(id:int):
+    for usr in usuarios:
+        if usr["id"] == id:
+            usuarios.remove(usr)
+            return {'message':'Usuario eliminado'}
+    raise HTTPException(status_code=400, detail='Usuario no registrado')
 
 
 
